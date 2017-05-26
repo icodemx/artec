d798e1df5847e687ed14b55bd1cd0f1e1aad6290
m4ruri <maruri.dg@gmail.com>
2017-05-26 13:23:33 -0500

    Estilización para dispositivos mas pequeños v1

--- themes/artec/assets/js/app.js
+++ themes/artec/assets/js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+    $('#Show-menu').click(function () {
+        var divheader = $(this).closest('#Header');
+        if (divheader.hasClass('Active')) {
+            divheader.removeClass('Active');
+        } else {
+            divheader.addClass('Active');
+        }
+    });
+
 });
