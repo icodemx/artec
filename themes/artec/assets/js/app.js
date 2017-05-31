a752b8de877c22c3511df5f8dc7f5f46279641fd
m4ruri <maruri.dg@gmail.com>
2017-05-31 11:03:42 -0500

    Reparación de accion de menu en movil

--- themes/artec/assets/js/app.js
+++ themes/artec/assets/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $('#Show-menu').click(function () {
-        var divheader = $(this).closest('#Header');
+        var divheader = $(this).closest('header');
         if (divheader.hasClass('Active')) {
             divheader.removeClass('Active');
         } else {
